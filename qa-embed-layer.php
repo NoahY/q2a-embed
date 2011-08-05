823f1c4d81b1782d294ee205b1942073c239c54a
NoahY <yuttadhammo@yahoo.com>
2011-08-05 08:37:50 +0530

    fixed end semi colin

--- qa-embed-layer.php
+++ qa-embed-layer.php
@@ -8,18 +8,18 @@
 		{
 			if (!empty($q_view['content'])){
 				$q_view = $this->embed_replace($q_view['content']);
-				qa_html_theme_base::q_view_content($q_view)
+				qa_html_theme_base::q_view_content($q_view);
 			}
 		}
 		function a_item_content($a_item)
 		{
 			$a_item = $this->embed_replace($a_item['content']);
-			qa_html_theme_base::a_item_content($a_item)
+			qa_html_theme_base::a_item_content($a_item);
 		}
 		function c_item_content($c_item)
 		{
 			$c_item = $this->embed_replace($a_item['content']);
-			qa_html_theme_base::c_item_content($c_item)
+			qa_html_theme_base::c_item_content($c_item);
 
 		}	
 		
