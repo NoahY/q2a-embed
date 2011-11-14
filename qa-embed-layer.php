1d6c2352cbb8fe0ff5709ac9f245d82860f6817e
NoahY <noahy57@gmail.com>
2011-11-14 08:18:31 +0530

    okay, use our own files... be safe

--- qa-embed-layer.php
+++ qa-embed-layer.php
@@ -8,8 +8,8 @@
 		{
 			qa_html_theme_base::head_custom();
 			if(qa_opt('embed_enable_thickbox')) { 
-				$this->output('<script type="text/javascript" src="http://jquery.com/demo/thickbox/thickbox-code/thickbox-compressed.js"></script>');
-				$this->output('<link rel="stylesheet" href="http://jquery.com/demo/thickbox/thickbox-code/thickbox.css" type="text/css" media="screen" />');
+				$this->output('<script type="text/javascript" src="'.QA_HTML_THEME_LAYER_URLTOROOT.'thickbox.js"></script>');
+				$this->output('<link rel="stylesheet" href="'.QA_HTML_THEME_LAYER_URLTOROOT.'thickbox.css" type="text/css" media="screen" />');
 			}
 		}
 		function q_view_content($q_view)
