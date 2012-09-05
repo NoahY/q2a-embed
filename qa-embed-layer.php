6acfa3bb9b42cd3321cdaa28b306101e2125d7e3
NoahY <noahy57@gmail.com>
2012-09-05 08:15:31 +0530

    added youtu.be link compat

--- qa-embed-layer.php
+++ qa-embed-layer.php
@@ -45,31 +45,65 @@
 			$h2 = qa_opt('embed_image_height');
 			
 			$types = array(
-				'youtube'=>array('https{0,1}:\/\/w{0,3}\.*youtube\.com\/watch\?\S*v=([A-Za-z0-9_-]+)[^< ]*','<iframe width="'.$w.'" height="'.$h.'" src="http://www.youtube.com/embed/$1?wmode=transparent" frameborder="0" allowfullscreen></iframe>'),
-				'vimeo'=>array('https{0,1}:\/\/w{0,3}\.*vimeo\.com\/([0-9]+)[^< ]*','<iframe src="http://player.vimeo.com/video/22775189?title=0&amp;byline=0&amp;portrait=0&wmode=transparent" width="'.$w.'" height="'.$h.'" frameborder="0"></iframe>'),
-				'metacafe'=>array('https{0,1}:\/\/w{0,3}\.*metacafe\.com\/watch\/([0-9]+)\/([a-z0-9_]+)[^< ]*','<embed flashVars="playerVars=showStats=no|autoPlay=no" src="http://www.metacafe.com/fplayer/$1/$2.swf" width="'.$w.'" height="'.$h.'" wmode="transparent" allowFullScreen="true" allowScriptAccess="always" name="Metacafe_$1" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash"></embed>'),
-				'dailymotion'=>array('https{0,1}:\/\/w{0,3}\.*dailymotion\.com\/video\/([A-Za-z0-9]+)[^< ]*','<iframe frameborder="0" width="'.$w.'" height="'.$h.'" src="http://www.dailymotion.com/embed/video/$1?wmode=transparent"></iframe>'),
-				'image'=>array('(https*:\/\/[-\%_\/.a-zA-Z0-9]+\.(png|jpg|jpeg|gif|bmp))[^< ]*','<img src="$1" style="max-width:'.$w2.'px;max-height:'.$h2.'px" />','img'),
-				'mp3'=>array('(https*:\/\/[-\%_\/.a-zA-Z0-9]+\.mp3)[^< ]*','<embed type="application/x-shockwave-flash" flashvars="audioUrl=$1" src="http://www.google.com/reader/ui/3523697345-audio-player.swf" width="400" height="27" quality="best"></embed>','mp3'),
+				'youtube'=>array(
+					array(
+						'https{0,1}:\/\/w{0,3}\.*youtube\.com\/watch\?\S*v=([A-Za-z0-9_-]+)[^< ]*',
+						'<iframe width="'.$w.'" height="'.$h.'" src="http://www.youtube.com/embed/$1?wmode=transparent" frameborder="0" allowfullscreen></iframe>'
+					),
+					array(
+						'https{0,1}:\/\/w{0,3}\.*youtu\.be\/([A-Za-z0-9_-]+)[^< ]*',
+						'<iframe width="'.$w.'" height="'.$h.'" src="http://www.youtube.com/embed/$1?wmode=transparent" frameborder="0" allowfullscreen></iframe>'
+					)
+				),
+				'vimeo'=>array(
+					array(
+						'https{0,1}:\/\/w{0,3}\.*vimeo\.com\/([0-9]+)[^< ]*',
+						'<iframe src="http://player.vimeo.com/video/22775189?title=0&amp;byline=0&amp;portrait=0&wmode=transparent" width="'.$w.'" height="'.$h.'" frameborder="0"></iframe>')
+				),
+				'metacafe'=>array(
+					array(
+						'https{0,1}:\/\/w{0,3}\.*metacafe\.com\/watch\/([0-9]+)\/([a-z0-9_]+)[^< ]*',
+						'<embed flashVars="playerVars=showStats=no|autoPlay=no" src="http://www.metacafe.com/fplayer/$1/$2.swf" width="'.$w.'" height="'.$h.'" wmode="transparent" allowFullScreen="true" allowScriptAccess="always" name="Metacafe_$1" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash"></embed>'
+					)
+				),
+				'dailymotion'=>array(
+					array(
+						'https{0,1}:\/\/w{0,3}\.*dailymotion\.com\/video\/([A-Za-z0-9]+)[^< ]*',
+						'<iframe frameborder="0" width="'.$w.'" height="'.$h.'" src="http://www.dailymotion.com/embed/video/$1?wmode=transparent"></iframe>'
+					)
+				),
+				'image'=>array(
+					array(
+						'(https*:\/\/[-\%_\/.a-zA-Z0-9]+\.(png|jpg|jpeg|gif|bmp))[^< ]*',
+						'<img src="$1" style="max-width:'.$w2.'px;max-height:'.$h2.'px" />','img'
+					)
+				),
+				'mp3'=>array(
+					array(
+						'(https*:\/\/[-\%_\/.a-zA-Z0-9]+\.mp3)[^< ]*',
+						'<embed type="application/x-shockwave-flash" flashvars="audioUrl=$1" src="http://www.google.com/reader/ui/3523697345-audio-player.swf" width="400" height="27" quality="best"></embed>','mp3'
+					)
+				),
 			);
 
-			foreach($types as $t => $r) {
-				
-				if( (!isset($r[2]) && !qa_opt('embed_enable')) || (isset($r[2]) && !qa_opt('embed_enable_'.$r[2])) ) continue;
-				
-				if(isset($r[2]) && @$r[2] == 'img' && qa_opt('embed_enable_thickbox') && preg_match('/MSIE [5-7]/',$_SERVER['HTTP_USER_AGENT']) == 0) {
-					preg_match_all('/'.$r[0].'/',$text,$imga);
-					if(!empty($imga)) {
-						foreach($imga[1] as $img) {
-							$replace = '<a href="'.$img.'" class="thickbox"><img  src="'.$img.'" style="max-width:'.$w2.'px;max-height:'.$h2.'px" /></a>';
-							$text = preg_replace('|<a[^>]+>'.$img.'</a>|i',$replace,$text);
-							$text = preg_replace('|(?<![\'"=])'.$img.'|i',$replace,$text);
+			foreach($types as $t => $ra) {
+				foreach($ra as $r) {
+					if( (!isset($r[2]) && !qa_opt('embed_enable')) || (isset($r[2]) && !qa_opt('embed_enable_'.$r[2])) ) continue;
+					
+					if(isset($r[2]) && @$r[2] == 'img' && qa_opt('embed_enable_thickbox') && preg_match('/MSIE [5-7]/',$_SERVER['HTTP_USER_AGENT']) == 0) {
+						preg_match_all('/'.$r[0].'/',$text,$imga);
+						if(!empty($imga)) {
+							foreach($imga[1] as $img) {
+								$replace = '<a href="'.$img.'" class="thickbox"><img  src="'.$img.'" style="max-width:'.$w2.'px;max-height:'.$h2.'px" /></a>';
+								$text = preg_replace('|<a[^>]+>'.$img.'</a>|i',$replace,$text);
+								$text = preg_replace('|(?<![\'"=])'.$img.'|i',$replace,$text);
+							}
 						}
+						continue;
 					}
-					continue;
+					$text = preg_replace('/<a[^>]+>'.$r[0].'<\/a>/i',$r[1],$text);
+					$text = preg_replace('/(?<![\'"=])'.$r[0].'/i',$r[1],$text);
 				}
-				$text = preg_replace('/<a[^>]+>'.$r[0].'<\/a>/i',$r[1],$text);
-				$text = preg_replace('/(?<![\'"=])'.$r[0].'/i',$r[1],$text);
 			}
 			return $text;
 		}
--- qa-plugin.php
+++ qa-plugin.php
@@ -5,7 +5,7 @@
         Plugin URI: https://github.com/NoahY/q2a-embed
         Plugin Update Check URI: https://github.com/NoahY/q2a-embed/raw/master/qa-plugin.php
         Plugin Description: Embed Video, Images and MP3 files
-        Plugin Version: 1.2
+        Plugin Version: 1.3
         Plugin Date: 2011-07-30
         Plugin Author: NoahY
         Plugin Author URI:                              
